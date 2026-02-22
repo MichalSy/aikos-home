04e1d651da6421f68b936736ed2961a10f4d5727
Aiko <aiko@local>
2026-02-22 13:04:47 +0100

    feat: update core to 1.0.30 with SSR-default config

--- next.config.js
+++ next.config.js
@@ -3,4 +3,12 @@ const { withAikoApp } = require('@michalsy/aiko-webapp-core/next-config')
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = withAikoApp(nextConfig)
+// SSR by default - only specified pages use Client Components ('use client')
+module.exports = withAikoApp(nextConfig, {
+  // Pages that need client-side interactivity (tab switching, state, etc.)
+  clientPages: [
+    '/dashboard/kanban',
+    '/dashboard/settings', 
+    '/dashboard/inventory'
+  ]
+})
